5980ee2f4a7070b78ef2e1b199dc90f4bfae5008
Guillaume Barré <guillaume.barre@ac-versailles.fr>
2025-11-28 11:09:03 +0100

    feat: update version to 1.8.0 and add new regulations for November 2025

--- src/environments/reglements.ts
+++ src/environments/reglements.ts
@@ -1,6 +1,134 @@
 import { Rules } from 'src/app/models/rules.model';
 
 export const reglements: Rules = {
+  '2025-11-v20251126': {
+    name: 'Novembre 2025 v20251126 (saison sportive 2026)',
+    topics: [
+      {
+        title: 'I.C.1 – Les compétiteurs étrangers',
+        content:
+          '<ul><li>Clarification sur la participation des étrangers en championnats nationaux.</li><li>Mention nouvelle : <em>Les étrangers apparaissent dans le classement des qualifications mais ne participent pas aux phases finales.</em></li><li>Ajout : <em>En Championnat de France Jeune par équipe de club, les archers étrangers ne sont pas autorisés.</em></li></ul>',
+        link: 'https://www.fftiralarc.org/DOCUMENTS/ARBITRES/Reglements_Sportifs_Arbitrage_Saison_Sportive_2026_Version_20251126.pdf',
+      },
+      {
+        title: 'I.C.5.1.2 – Sélection',
+        content:
+          '<ul><li>Refonte complète des règles de sélection.</li><li>Processus unifié : pré-inscription, modalités annuelles, quotas, extraction à J-10.</li><li>Suppression de la zone tampon détaillée.</li><li>Disparition des distinctions désuètes entre Elite et autres formats.</li><li>Texte réécrit pour couvrir toutes les disciplines de manière cohérente.</li></ul>',
+        link: 'https://www.fftiralarc.org/DOCUMENTS/ARBITRES/Reglements_Sportifs_Arbitrage_Saison_Sportive_2026_Version_20251126.pdf#%5B%7B%22num%22%3A408%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22XYZ%22%7D%2C54.48%2C258.96%2C0%5D',
+      },
+      {
+        title: 'I.D.3 – Substances dopantes et alcool',
+        content:
+          '<ul><li>Titre modifié : passage à <em>Substances dopantes et alcool</em>.</li><li>Section restructurée en sous-chapitres : lutte antidopage, contrôles hors compétition, alcool.</li><li>Réécriture complète du chapitre sur l’alcool : procédure arbitre, gestion des incidents, obligations fédérales.</li><li>Clarification des contrôles hors compétition.</li></ul>',
+        link: 'https://www.fftiralarc.org/DOCUMENTS/ARBITRES/Reglements_Sportifs_Arbitrage_Saison_Sportive_2026_Version_20251126.pdf#%5B%7B%22num%22%3A548%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22XYZ%22%7D%2C75.6%2C486%2C0%5D',
+      },
+      {
+        title: 'I.G.3.2 – Arc à Poulies – Le viseur',
+        content:
+          "Clarification sur l'utilisation des points de visées multiples",
+        link: 'https://www.fftiralarc.org/DOCUMENTS/ARBITRES/Reglements_Sportifs_Arbitrage_Saison_Sportive_2026_Version_20251126.pdf#%5B%7B%22num%22%3A704%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22XYZ%22%7D%2C54.48%2C415.44%2C0%5D',
+      },
+      {
+        title: 'I.G.3.4 – Arc Nu (Barebow) – Stabilisateurs et amortisseurs',
+        content:
+          "Clarification sur le passage dans l'anneau : <em>sans plier les amortisseurs</em>",
+        link: 'https://www.fftiralarc.org/DOCUMENTS/ARBITRES/Reglements_Sportifs_Arbitrage_Saison_Sportive_2026_Version_20251126.pdf#%5B%7B%22num%22%3A722%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22XYZ%22%7D%2C54.48%2C785.28%2C0%5D',
+      },
+
+      {
+        title: 'II.1.B.5.4 – Temps de tir et séquence de tir',
+        content: 'Ajout du temps de tir <em>international</em>.',
+        link: 'https://www.fftiralarc.org/DOCUMENTS/ARBITRES/Reglements_Sportifs_Arbitrage_Saison_Sportive_2026_Version_20251126.pdf#%5B%7B%22num%22%3A1083%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22XYZ%22%7D%2C54.48%2C250.08%2C0%5D',
+        discipline: "Tir à l'arc Extérieur",
+      },
+
+      {
+        title: 'II.1.C.1.1 – Championnat de France Élite TAE',
+        content:
+          'Aucun changement détécté par rapport à la version précédente. (???)',
+        link: 'https://www.fftiralarc.org/DOCUMENTS/ARBITRES/Reglements_Sportifs_Arbitrage_Saison_Sportive_2026_Version_20251126.pdf#%5B%7B%22num%22%3A1309%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22XYZ%22%7D%2C54.48%2C748.56%2C0%5D',
+        discipline: "Tir à l'arc Extérieur",
+      },
+      {
+        title: 'II.1.C.3.1 – Championnat de France D1 Arc Classique',
+        content:
+          '<ul><li>La Saison Régulière (SR) se réalise sur les <strong>deux premières manches</strong> par la rencontre de toutes les équipes entre elles <strong>une fois</strong>, soit <strong>15 rencontres</strong>.</li><li>Pour les 8 premières équipes de la SR, une phase de play-off est organisée.</li><li>Les 4 premières équipes au classement de la phase de Play-Off sont qualifiées pour participer à la phase finale du Championnat de France de D1.</li></ul>',
+        link: 'https://www.fftiralarc.org/DOCUMENTS/ARBITRES/Reglements_Sportifs_Arbitrage_Saison_Sportive_2026_Version_20251126.pdf#%5B%7B%22num%22%3A1369%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22XYZ%22%7D%2C54.48%2C772.08%2C0%5D',
+        discipline: "Tir à l'arc Extérieur",
+      },
+      {
+        title: 'II.1.C.3.2 – Championnat de France D2',
+        content:
+          'Le déroulement de la saison se fait à présent en 2 manches permettant la rencontre de toutes les équipes entre elles <strong>deux fois</strong> dans la saison, soit 14 matchs par équipe répartis en 7 matchs par manche.',
+        link: 'https://www.fftiralarc.org/DOCUMENTS/ARBITRES/Reglements_Sportifs_Arbitrage_Saison_Sportive_2026_Version_20251126.pdf#%5B%7B%22num%22%3A1402%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22XYZ%22%7D%2C54.48%2C772.08%2C0%5D',
+        discipline: "Tir à l'arc Extérieur",
+      },
+      {
+        title: 'II.1.C.3.4 – Championnat de France Jeune par équipe de club',
+        content: 'Précisions sur le déroulement de la phase de qualification.',
+        link: 'https://www.fftiralarc.org/DOCUMENTS/ARBITRES/Reglements_Sportifs_Arbitrage_Saison_Sportive_2026_Version_20251126.pdf#%5B%7B%22num%22%3A1419%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22XYZ%22%7D%2C54.48%2C772.08%2C0%5D',
+        discipline: "Tir à l'arc Extérieur",
+      },
+      {
+        title: 'II.1.C.3.5 – Championnat de France D1 Arc à Poulies',
+        content:
+          "Le Championnat de France D1 Arc à Poulies, se dispute en deux ou 3 phases sur la durée d'une saison sportive :<ul><li>Une phase de Saison Régulière (SR) sur les 2 premières manches, pour les hommes et sur les 3 manches pour les femmes</li><li>Une phase de Play-Off ou de Play-Down chez hommes en fonction du classement obtenu par les équipes sur la SR.</li><li>Une phase finale pour l’attribution des places sur le podium chez les hommes et chez les femmes.</li></ul>",
+        link: 'https://www.fftiralarc.org/DOCUMENTS/ARBITRES/Reglements_Sportifs_Arbitrage_Saison_Sportive_2026_Version_20251126.pdf#%5B%7B%22num%22%3A1497%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22XYZ%22%7D%2C54.48%2C772.08%2C0%5D',
+        discipline: "Tir à l'arc Extérieur",
+      },
+      {
+        title: 'II.1.C.4 – Tournois Nationaux Jeunes',
+        content:
+          'Ouverture de U13 à U21 en classique et de U18 à U21 en arc à poulies.',
+        link: 'https://www.fftiralarc.org/DOCUMENTS/ARBITRES/Reglements_Sportifs_Arbitrage_Saison_Sportive_2026_Version_20251126.pdf#%5B%7B%22num%22%3A1524%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22XYZ%22%7D%2C54.48%2C670.8%2C0%5D',
+        discipline: "Tir à l'arc Extérieur",
+      },
+      {
+        title:
+          "II.2.C.1.1 – Championnat de France Adulte Tir à 18m par Catégorie d'âge",
+        content: 'Plus de mention de championnat de France "élite".',
+        link: 'https://www.fftiralarc.org/DOCUMENTS/ARBITRES/Reglements_Sportifs_Arbitrage_Saison_Sportive_2026_Version_20251126.pdf#%5B%7B%22num%22%3A1937%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22XYZ%22%7D%2C54.48%2C785.28%2C0%5D',
+        discipline: 'Tir à 18m',
+      },
+      {
+        title: 'II.3.B.6.3 – Résolution des égalités',
+        content:
+          '<ul><li>Explications détaillées sur la procédure des tirs de barrages.</li><li>Ajout de la procédure pour le double-mixte.</li></ul>',
+        link: 'https://www.fftiralarc.org/DOCUMENTS/ARBITRES/Reglements_Sportifs_Arbitrage_Saison_Sportive_2026_Version_20251126.pdf#%5B%7B%22num%22%3A2099%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22XYZ%22%7D%2C289.2%2C687.36%2C0%5D',
+        discipline: 'Tir en campagne',
+      },
+      {
+        title: 'II.3.C – Règlements spécifiques des épreuves fédérales',
+        content:
+          '<ul><li>Réorganisation des sections.</li><li>Suppression de la section concernant la sélection pour le championnat de France élite.</li></ul>',
+        link: 'https://www.fftiralarc.org/DOCUMENTS/ARBITRES/Reglements_Sportifs_Arbitrage_Saison_Sportive_2026_Version_20251126.pdf#%5B%7B%22num%22%3A2161%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22XYZ%22%7D%2C54.48%2C708.72%2C0%5D',
+        discipline: 'Tir en campagne',
+      },
+      {
+        title: 'II.5.B.6.5.1 – Zones marquantes suivant le type de gibiers',
+        content:
+          'Clarifications sur les zones marquantes en fonction des types de gibiers.',
+        link: 'https://www.fftiralarc.org/DOCUMENTS/ARBITRES/Reglements_Sportifs_Arbitrage_Saison_Sportive_2026_Version_20251126.pdf#%5B%7B%22num%22%3A2573%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22XYZ%22%7D%2C54.48%2C785.28%2C0%5D',
+        discipline: 'Tir Nature',
+      },
+
+      {
+        title: 'II.6.B.8 – Résolution des égalités',
+        content:
+          '<ul><li>Le départage se fait uniquement au nombre de 11 puis de 10.</li><li>Ajout de la procédure de départage pour les phases finales, progressions et attribution des médailles en championnat.</li></ul>',
+        link: 'https://www.fftiralarc.org/DOCUMENTS/ARBITRES/Reglements_Sportifs_Arbitrage_Saison_Sportive_2026_Version_20251126.pdf#%5B%7B%22num%22%3A2761%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22XYZ%22%7D%2C68.64%2C785.28%2C0%5D',
+        discipline: 'Tir 3D',
+      },
+
+      {
+        title: 'II.6.C – Règlements spécifiques des épreuves fédérales',
+        content:
+          '<ul><li>Réorganisation des sections.</li><li>Suppression de la section concernant la sélection pour le championnat de France élite.</li><li>Réorganisation des jours de compétition pour le tir en individuel.</li></ul>',
+        link: 'https://www.fftiralarc.org/DOCUMENTS/ARBITRES/Reglements_Sportifs_Arbitrage_Saison_Sportive_2026_Version_20251126.pdf#%5B%7B%22num%22%3A2761%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22XYZ%22%7D%2C68.64%2C720.48%2C0%5D',
+        discipline: 'Tir 3D',
+      },
+    ],
+  },
   '2025-07-v2': {
     name: 'Juillet 2025 v2 (saison sportive 2026)',
     topics: [
